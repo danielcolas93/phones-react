6663f55f078c96227f018d58b6dc0c32f02f121b
Daniel Colas <danielenrique93@gmail.com>
2019-10-20 23:15:09 +0200

    App, index and serviceWorker

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeScreen from './pages/Home';
+import PhoneModal from './components/PhoneModal';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+      <PhoneModal />
+    </div>
+  );
+}
+
+export default App;
