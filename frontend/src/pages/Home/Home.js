fd1c5ae9b1feae9629b7e6ac05c039691a869cf8
Daniel Colas <danielenrique93@gmail.com>
2019-10-20 23:16:23 +0200

    Home page with PhoneListSecion created

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import PhoneListSection from './PhoneListSection/PhoneListSection';
+
+const HomeScreen = () => (
+  <Fragment>
+    <PhoneListSection />
+  </Fragment>
+);
+
+export default HomeScreen;
